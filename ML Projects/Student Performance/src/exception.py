2ee77e57c0ddfc287c1c15a2d6468a2446503426
Prasad Verulkar <prasadvverulkar@gmail.com>
2025-03-11 16:31:29 +0530

    Deployment configuration

--- ML Projects/Student Performance/src/exception.py
+++ ML Projects/Student Performance/src/exception.py
@@ -1,6 +1,6 @@
 import sys
 # sys.path.append(r'C:\Udemy\Projects\ML Projects\Student Performance\src')
-from logger import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
